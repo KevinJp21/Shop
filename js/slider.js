ec5151910aa3eb8c192b41750ce2c11cecfe84e3
KevinJp21 <kevinjp821@gmail.comm>
2023-01-10 17:49:50 -0500

    se ajustó la image del slider

--- js/slider.js
+++ js/slider.js
@@ -1,20 +1,26 @@
 const slider = document.querySelector("#slider");
+const text = document.querySelector("#text");
 let sliderSection = document.querySelectorAll(".slider_section");
 let sliderSectionLast = sliderSection[sliderSection.length-1];
+let sliderText = document.querySelectorAll(".central_text");
+let sliderTextLast = sliderText[sliderText.length-1];
 
 const btnLeft = document.querySelector("#btn-left");
 const btnRight = document.querySelector("#btn-right");
 
 slider.insertAdjacentElement('afterbegin',sliderSectionLast);/*Mueve la ultima imagen al inicio */
-
 function Next(){
     let sliderSectionFirst = document.querySelectorAll(".slider_section")[0];
-    slider.style.margineft = "-200%";
+    slider.style.margineft = "-100%";
+
     
 
     setTimeout(function(){
         slider.insertAdjacentElement('beforeend',sliderSectionFirst);
+        text.insertAdjacentElement('beforeend',sliderTextFirs)
         slider.style.marginLeft = "-100%";
+
+    
     },500);
 }
 
